d1bb11c3c0f67897544efbe46e6e8f9c46fa566f
yeahhh1e <cakejuuu@gmail.com>
2025-12-27 13:37:32 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/example/kankan/KanKanApplication.java
@@ -0,0 +1,13 @@
+package com.example.kankan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanKanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanKanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kankan/KanKanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kankan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanKanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
