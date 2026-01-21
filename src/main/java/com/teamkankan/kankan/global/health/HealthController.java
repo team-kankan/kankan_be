07265f7b252d1f2a4e9c8a1d9b9c6b571ded578e
yeahhh1e <cakejuuu@gmail.com>
2026-01-21 17:17:09 +0900

    chore: health controller 글로벌 패키지로 이동

--- src/main/java/com/teamkankan/kankan/global/health/HealthController.java
+++ src/main/java/com/teamkankan/kankan/global/health/HealthController.java
@@ -1,4 +1,4 @@
-package com.teamkankan.kankan;
+package com.teamkankan.kankan.global.health;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
